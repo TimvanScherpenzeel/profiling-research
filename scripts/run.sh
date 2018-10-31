0917cf7b01ca498e89704cd11aabd1d6d6023673
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-10-31 20:04:06 +0100

    fix various commands and outputs

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,9 +8,7 @@
 LOCATION="${1:-http://localhost:8080}"
 LOG_DIRECTORY="${2:-logs}"
 LOG_OUTPUT="${3:-logs/chrome_canary_output.log}"
-LOG_OUTPUT_V8="${4:-logs/chrome_canary_output_v8.log}"
 LOG_ERROR="${5:-logs/chrome_canary_error.log}"
-JS_FLAGS="--trace-file-names --trace-opt --trace-deopt --trace-ic --prof --print-bytecode --print-opt-source --code-comments --no-concurrent_recompilation"
 
 # Helper functions
 
@@ -34,8 +32,7 @@ run() {
     # Opening chrome://tracing is not allowed from the command line
     echo -e "Please open \"chrome://tracing\" to start V8 tracing in a new browser tab\n"
 
-    # Use Chrome Canary binary if available
-    /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary $LOCATION --incognito --no-sandbox --allow-file-access-from-files --logfile=$LOG_OUTPUT_V8 --js-flags=$JS_FLAGS > $LOG_OUTPUT 2> $LOG_ERROR
+    /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary $LOCATION --incognito --no-sandbox --allow-file-access-from-files --js-flags="--trace-file-names --trace-opt --trace-deopt --print-opt-source --code-comments" > $LOG_OUTPUT 2> $LOG_ERROR
 }
 
 # Main script
