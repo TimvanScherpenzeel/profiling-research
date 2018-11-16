a04f329709361dd4d5a29cdd0a8a4a35739b122f
Tim van Scherpenzeel <timvanscherpenzeel@Tims-MacBook-Pro.local>
2018-11-16 11:10:58 +0100

    allow to override tmp profile default directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,16 +3,14 @@
 # Script to start the Chrome Canary V8 profiler
 
 # Configuration
-
-# Temporary profile
-BASE_TEMP_DIR=/tmp
-TEMP_DIR=$(mktemp -d $BASE_TEMP_DIR/google-chome.XXXXXXX)
-
-# Fall back to regular chrome if Canary doesn't exist
 LOCATION="${1:-http://localhost:8080}"
 LOG_DIRECTORY="${2:-logs}"
 LOG_OUTPUT="${3:-logs/chrome_canary_output.log}"
-LOG_ERROR="${5:-logs/chrome_canary_error.log}"
+LOG_ERROR="${4:-logs/chrome_canary_error.log}"
+BASE_TEMP_PROFILE_DIR="${5:-/tmp}"
+
+# Temporary profile
+TEMP_PROFILE_DIR=$(mktemp -d $BASE_TEMP_PROFILE_DIR/google-chome.XXXXXXX)
 
 # Helper functions
 
@@ -33,16 +31,16 @@ run() {
 
     echo -e "Starting Chrome Canary with custom profiling flags\n"
 
-    echo -e "Created temporary profile folder in $TEMP_DIR"
+    echo -e "Created temporary profile folder in $TEMP_PROFILE_DIR"
 
     # Opening chrome://tracing is not allowed from the command line
     echo -e "Please open \"chrome://tracing\" to start V8 tracing in a new browser tab\n"
 
-    /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary $LOCATION --incognito --disable-gpu-vsync --disable-frame-rate-limit --ignore-gpu-blacklist --user-data-dir=$TEMP_DIR --no-first-run --js-flags="--trace-file-names --trace-opt --trace-deopt --print-opt-source --code-comments" 1> $LOG_OUTPUT 2> $LOG_ERROR
+    /Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary $LOCATION --incognito --disable-gpu-vsync --disable-frame-rate-limit --ignore-gpu-blacklist --user-data-dir=$TEMP_PROFILE_DIR --no-first-run --js-flags="--trace-file-names --trace-opt --trace-deopt --print-opt-source --code-comments" 1> $LOG_OUTPUT 2> $LOG_ERROR
 
-    echo -e "Cleaning up temporary profile folder in $TEMP_DIR"
+    echo -e "Cleaning up temporary profile folder in $TEMP_PROFILE_DIR"
 
-    rm -rf $TEMP_DIR
+    rm -rf $TEMP_PROFILE_DIR
 }
 
 # Main script
